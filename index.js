5c3bbfe7eb49f9605637a455c09b91d945e4a1ad
frankfil <frank.filius@gmail.com>
2017-11-13 22:24:29 +0100

    Add bodyparser middleware so the request body gets parsed

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const { Group } = require('./models')
 
 const PORT = process.env.PORT || 3030
 
 let app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.get('/groups', (req, res, next) => {
   Group.find()
 
@@ -12,6 +15,23 @@ app.get('/groups', (req, res, next) => {
 
     .then((groups) => res.json(groups))
 
+    .catch((error) => next(error))
+})
+
+app.get('/groups/:id', (req, res, next) => {
+  const id = req.params.id
+  Group.findById(id)
+    .then((group) => {
+      if (!group) { return next() }
+      res.json(group)
+    })
+    .catch((error) => next(error))
+})
+
+app.post('/groups', passport.authorize('jwt', { session: false }), (req, res, next) => {
+  let newGroup = req.body
+  Group.create(newGroup)
+    .then((group) => res.json(group))
     .catch((error) => next(error))
 })
 
