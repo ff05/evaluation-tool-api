6a7d511b92ef5cb428d0ebbc2b6f4fd6a44a4435
frankfil <frank.filius@gmail.com>
2017-11-15 08:29:18 +0100

    remove explicit id

--- models/student.js
+++ models/student.js
@@ -2,13 +2,9 @@ const mongoose = require('../config/database')
 const { Schema }  = mongoose
 
 const studentSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: 'students' },
   name: { type: String, required: true },
   picture: { type: String, required: true },
-  day: {
-    eval: String,
-    summary: String
-  }
+  days: [{ day: Date, eval: String, summary: String }]
 })
 
 module.exports = mongoose.model('students', studentSchema)
